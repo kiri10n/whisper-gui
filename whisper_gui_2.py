fdbb43126fc135a43504675ac846187cea9c7da4
kiritohaze <kiritohaze@gmail.com>
2024-04-19 17:26:34 +0900

    一応動いた。改行コードをLFからCRLFへ。

--- whisper_gui_2.py
+++ whisper_gui_2.py
@@ -5,12 +5,30 @@ import numpy as np
 import gradio as gr
 import whisper
 
-model = whisper.load_model("base")
+model = whisper.load_model("large")
 
-def transcribe_audio():
-    result = model.transcribe("./flagged/audio/")
-    return result["text"]
+# def transcribe_audio():
+#     result = model.transcribe("./flagged/audio/")
+#     return result["text"]
 
+def transcribe_audio(audio):
+    audio = whisper.load_audio(audio)
+    audio = whisper.pad_or_trim(audio)
+
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+    # detect the spoken language
+    _, probs = model.detect_language(mel)
+    print(f"Detected language: {max(probs, key=probs.get)}")
+
+    # decode the audio
+    options = whisper.DecodingOptions(fp16=False)
+    result = whisper.decode(model, mel, options)
+
+    # print the recognized text
+    print(result.text)
+    return result.text
 
 input_audio = gr.Audio(
     sources=["microphone"],
@@ -20,11 +38,12 @@ input_audio = gr.Audio(
         skip_length=2,
         show_controls=False,
     ),
+    type="filepath"
 )
 demo = gr.Interface(
     fn=transcribe_audio,
-    inputs=input_audio,
-    outputs="audio"
+    inputs=[input_audio],
+    outputs="textbox"
 )
 
 if __name__ == "__main__":
